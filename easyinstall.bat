4aa04f468ac1d778e7f9e98c3476a4d7016a410f
tonyxforce <horich.dominik@gmail.com>
2025-04-05 22:07:38 +0200

    delete temporary files after use

--- easyinstall.bat
+++ easyinstall.bat
@@ -7,5 +7,6 @@ cd ..
 
 curl https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py > get-platformio.py
 python3 get-platformio.py
+del get-platformio.py
 
 %userprofile%\.platformio\penv\Scripts\pio run --target=upload --upload-port=2.0.0.1
