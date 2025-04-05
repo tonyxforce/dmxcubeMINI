0836e48ce0511d8b3080a27ef20a3ebdde60b4ce
tonyxforce <horich.dominik@gmail.com>
2025-04-05 21:03:48 +0200

    Automatic install and update script

---
+++ easyinstall.bat
@@ -0,0 +1,11 @@
+winget install -e --id Python.Python.3.11
+winget install -e --id OpenJS.NodeJS
+
+cd scripts
+call npm install
+cd ..
+
+curl https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py > get-platformio.py
+python3 get-platformio.py
+
+%userprofile%\.platformio\penv\Scripts\pio run --target=upload --upload-port=2.0.0.1
